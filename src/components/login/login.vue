fe13bb079a1962e71963bb1a2e28b4779a6f1ca8
zuojianglong <3155481042@qq.com>
2019-08-19 21:13:06 +0800

    数据绑定及校验

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -2,19 +2,19 @@
   <div class="login">
       <el-card class="card">
           <img src="../../assets/img/logo_index.png" alt="">
-          <el-form>
-              <el-form-item>
-                  <el-input placeholder="请输入手机号"></el-input>
+          <el-form :model="ruleForm" :rules="rules" ref="loginForm">
+              <el-form-item prop="mobile">
+                  <el-input v-model="ruleForm.mobile" placeholder="请输入手机号"></el-input>
               </el-form-item>
-              <el-form-item>
-                  <el-input style="width:240px" placeholder="请输入验证码"></el-input>
+              <el-form-item prop="code">
+                  <el-input v-model="ruleForm.code" style="width:240px" placeholder="请输入验证码"></el-input>
                   <el-button style="float:right">发送验证码</el-button>
               </el-form-item>
-              <el-form-item>
-                  <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+              <el-form-item prop="check">
+                  <el-checkbox v-model="ruleForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
               </el-form-item>
               <el-form-item>
-                  <el-button type="primary" style="width:100px">登录</el-button>
+                  <el-button @click="login" type="primary" style="width:100px">登录</el-button>
               </el-form-item>
           </el-form>
       </el-card>
@@ -22,8 +22,51 @@
 </template>
 
 <script>
+// import { log } from 'util'
 export default {
-
+  data () {
+    var func = function (rule, value, callback) {
+      if (value) {
+        callback()
+      } else {
+        callback(new Error('请同意协议'))
+      }
+    }
+    return {
+      ruleForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      rules: {
+        mobile: [{
+          required: true, message: '手机号不能为空', trigger: 'blur'
+        }, {
+          message: '手机号格式不对', pattern: /^1[3456789]\d{9}$/, trigger: 'blur'
+        }
+        ],
+        code: [{
+          required: true, message: '验证码不能为空', trigger: 'blur'
+        }, {
+          message: '验证码长度必须为6个数字', pattern: /^\d{6}$/, trigger: 'blur'
+        }
+        ],
+        check: [{
+          validator: func
+        }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate(isOK => {
+        if (isOK) {
+          console.log('前端校验成功')
+        }
+      })
+    }
+  }
 }
 </script>
 
