47a0d31dd79c35c9e73fa9ee0f873579a14550b7
zuojianglong <3155481042@qq.com>
2019-08-19 20:10:26 +0800

    页面布局及样式

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-      login
+  <div class="login">
+      <el-card class="card">
+          <img src="../../assets/img/logo_index.png" alt="">
+          <el-form>
+              <el-form-item>
+                  <el-input placeholder="请输入手机号"></el-input>
+              </el-form-item>
+              <el-form-item>
+                  <el-input style="width:240px" placeholder="请输入验证码"></el-input>
+                  <el-button style="float:right">发送验证码</el-button>
+              </el-form-item>
+              <el-form-item>
+                  <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+              </el-form-item>
+              <el-form-item>
+                  <el-button type="primary" style="width:100px">登录</el-button>
+              </el-form-item>
+          </el-form>
+      </el-card>
   </div>
 </template>
 
@@ -10,6 +27,24 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login {
+    background-image: url('../../assets/img/login_bg.jpg');
+    width: 100%;
+    height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    .card {
+        width: 400px;
+        height: 330px;
+        img {
+            width: 200px;
+            height: 45px;
+            margin-bottom: 20px;
+        }
+    }
+}
 </style>
