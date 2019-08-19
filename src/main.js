c8bc79c41b2ec2b88ba5c46f6838742b0534e37b
zuojianglong <3155481042@qq.com>
2019-08-19 21:27:03 +0800

    “安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less' // 引入重置的基础样式
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototype.$axios = axios
 new Vue({
   router,
   render: h => h(App)
