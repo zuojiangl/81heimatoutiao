7ef7470c4a613457d9055bab7093cd5ef741b831
zuojianglong <3155481042@qq.com>
2019-08-20 14:15:13 +0800

    主页布局

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
-    home
+    <!-- 最外层容器 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside>侧边栏</el-aside>
+      <!-- 内部容器区域 -->
+      <el-container>
+        <!-- 顶部容器 -->
+        <el-header>顶部</el-header>
+        <!-- 主要内容区域 -->
+        <el-main>主体</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
