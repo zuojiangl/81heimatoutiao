90cab21707d0133c61ef436675a5ad4b80b2599d
zuojianglong <3155481042@qq.com>
2019-08-20 19:55:00 +0800

    左侧导航菜单

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,7 +3,9 @@
     <!-- 最外层容器 -->
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside>侧边栏</el-aside>
+      <el-aside style="width:200px;height:100vh;background-color:#323745">
+        <layout-aside></layout-aside>
+      </el-aside>
       <!-- 内部容器区域 -->
       <el-container>
         <!-- 顶部容器 -->
@@ -18,11 +20,11 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
+import layoutAside from '../../components/home/layout-aside'
 export default {
   name: 'home',
   components: {
-
+    'layout-aside': layoutAside
   }
 }
 </script>
