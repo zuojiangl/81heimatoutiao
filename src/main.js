318bf012bc1bb848d781c9c9a098d905905ac61b
zuojianglong <3155481042@qq.com>
2019-08-21 21:18:31 +0800

    统一处理请求token

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import App from './App.vue'
 import router from './permission'
 import ElementUI from 'element-ui'
 import Component from './components/index'
-import axios from 'axios'
+// import axios from 'axios'
+import axios from './utils/axios.config'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less' // 引入重置的基础样式
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.use(Component)
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+// axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios
 new Vue({
   router,
---
+++ src/utils/axios.config.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+axios.interceptors.request.use(function (config) {
+  // 在发起请求做一些业务处理
+  // 进入配置的注入
+  // 获取token
+  let userInfo = window.localStorage.getItem('user-info')
+  let token = userInfo ? JSON.parse(userInfo).token : null
+  // 将token注入请求参数中
+  config.headers.Authorization = `Bearer ${token}`
+  return config
+}, function (error) {
+  // 请求失败处理
+  return Promise.reject(error)
+})
+export default axios
