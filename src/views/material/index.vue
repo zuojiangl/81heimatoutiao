7ac5c379b3a752ff3cd376358d941ebfe9ec9bf2
zuojianglong <3155481042@qq.com>
2019-08-23 00:20:34 +0800

    素材管理分页功能

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -5,28 +5,47 @@
       <!-- 面包屑具名插槽 -->
       <template slot="title">素材管理</template>
     </bread-crumb>
-    <el-tabs v-model="activeName" @tab-click='changeTab'>
-    <el-tab-pane label="全部素材" name="all">
-      <div class="imgList">
-        <el-card class="img-card" v-for="item in list" :key="item.id">
-          <img :src="item.url" alt="">
-          <el-row align="middle" class="operate" type="flex" justify="space-around">
-            <i class="el-icon-star-on"></i>
-            <i class="el-icon-delete-solid"></i>
-          </el-row>
-        </el-card>
-      </div>
-    </el-tab-pane>
-    <el-tab-pane label="收藏素材" name="collect">
-      <div class="imgList">
-        <el-card class="img-card" v-for="item in list" :key="item.id">
-          <img :src="item.url" alt="">
-          <el-row align="middle" class="operate" type="flex" justify="space-around">
-          </el-row>
-        </el-card>
-      </div>
-    </el-tab-pane>
-  </el-tabs>
+    <el-tabs v-model="activeName" @tab-click="changeTab">
+      <el-tab-pane label="全部素材" name="all">
+        <div class="imgList">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+            <el-row align="middle" class="operate" type="flex" justify="space-around">
+              <i class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+            </el-row>
+          </el-card>
+        </div>
+        <el-row type="flex" justify="center">
+          <el-pagination
+            :page-size="page.pageSize"
+            :current-page="page.currentPage"
+            :total="page.total"
+            @current-change="changePage"
+            background
+            layout="prev, pager, next"
+          ></el-pagination>
+        </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="收藏素材" name="collect">
+        <div class="imgList">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+            <el-row align="middle" class="operate" type="flex" justify="space-around"></el-row>
+          </el-card>
+        </div>
+        <el-row type="flex" justify="center">
+          <el-pagination
+            :page-size="page.pageSize"
+            :current-page="page.currentPage"
+            :total="page.total"
+            @current-change="changePage"
+            background
+            layout="prev, pager, next"
+          ></el-pagination>
+        </el-row>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
@@ -35,19 +54,34 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: []
+      list: [],
+      page: {
+        pageSize: 12,
+        total: 0,
+        currentPage: 1
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getMaterial()
+    },
     changeTab () {
+      this.page.currentPage = 1
       this.getMaterial()
     },
     getMaterial () {
+      let pageParams = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize
+      }
       this.$axios({
         url: '/user/images',
-        params: { collect: this.activeName === 'collect' }
+        params: { collect: this.activeName === 'collect', ...pageParams }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
   },
@@ -78,6 +112,9 @@ export default {
       height: 30px;
       width: 100%;
       background-color: #f4f5f6;
+      i {
+        font-size: 22px;
+      }
     }
   }
 }
