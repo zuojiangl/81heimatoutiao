cb29813a2326847cb042e7ca55d9647d0d636b53
zuojianglong <3155481042@qq.com>
2019-08-22 22:00:19 +0800

    评论分页及请求

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -2,7 +2,7 @@
   <el-card>
     <!-- el-card具名插槽 -->
     <bread-crumb slot="header">
-    <!-- 面包屑具名插槽 -->
+      <!-- 面包屑具名插槽 -->
       <template slot="title">评论列表</template>
     </bread-crumb>
     <!-- el-card匿名插槽 -->
@@ -15,10 +15,17 @@
       <el-table-column label="操作">
         <template slot-scope="obj">
           <el-button type="text">修改</el-button>
-          <el-button @click="closeOrOpen(obj.row)" :style="{color: obj.row.comment_status ? '#E6A23C' : '#409EFF'}" type="text">{{obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
+          <el-button
+            @click="closeOrOpen(obj.row)"
+            :style="{color: obj.row.comment_status ? '#E6A23C' : '#409EFF'}"
+            type="text"
+          >{{obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <el-row type="flex" justify="center" style="margin:20px 0">
+      <el-pagination :page-size="page.pageSize" :current-page="page.currentPage"  :total="page.total" @current-change="changePage" background layout="prev, pager, next"></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -26,10 +33,19 @@
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      page: {
+        pageSize: 10,
+        total: 0,
+        currentPage: 1
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getComments()
+    },
     closeOrOpen (row) {
       let mess = row.comment_status ? '关闭' : '打开'
       this.$confirm(`您确定要${mess}评论吗`, '提示').then(() => {
@@ -45,13 +61,17 @@ export default {
     },
     // 查询评论列表数据
     getComments () {
+      let pageParams = { page: this.page.currentPage,
+        per_page: this.page.pageSize }
       this.$axios({
         url: '/articles',
         params: {
-          response_type: 'comment'
+          response_type: 'comment',
+          ...pageParams
         }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count// 获取总页数
       })
     },
     formatter (row, column, cellValue, index) {
@@ -65,5 +85,4 @@ export default {
 </script>
 
 <style>
-
 </style>
