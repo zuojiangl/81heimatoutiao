d9d1fc8c4b3a9eecae357c07841b6efb97d756c3
zuojianglong <3155481042@qq.com>
2019-08-24 15:55:18 +0800

    发布文章校验逻辑

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,23 +3,23 @@
     <bread-crumb slot="header">
       <template slot="title">发布文章</template>
     </bread-crumb>
-    <el-form>
-      <el-form-item label="标题">
-        <el-input style="width:400px" placeholder="文章名称"></el-input>
+    <el-form ref="myForm" :model="formData" :rules="rules" label-width="50px" class="publish-form">
+      <el-form-item prop="title" label="标题">
+        <el-input style="width:400px" placeholder="文章名称" v-model="formData.title"></el-input>
       </el-form-item>
-      <el-form-item label="内容">
-        <el-input type="textarea" :rows="5" placeholder="请输入内容"></el-input>
+      <el-form-item prop="content" label="内容">
+        <el-input v-model="formData.content" type="textarea" :rows="5" placeholder="请输入内容"></el-input>
       </el-form-item>
-      <el-form-item label="封面">
+      <el-form-item v-model="formData.cover" prop="cover" label="封面">
         <el-radio-group v-model="radio">
           <el-radio :label="1">单图</el-radio>
           <el-radio :label="2">三图</el-radio>
-          <el-radio :label="3">无图</el-radio>
-          <el-radio :label="4">自动</el-radio>
+          <el-radio :label="0">无图</el-radio>
+          <el-radio :label="-1">自动</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="频道">
-        <el-select v-model="channels.id" placeholder="请选择">
+      <el-form-item prop="channel_id" label="频道">
+        <el-select v-model="formData.channel_id" placeholder="请选择">
           <el-option
             v-for="item in channels"
             :key="item.id"
@@ -27,6 +27,10 @@
             :value="item.id"
           ></el-option>
         </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="publish" type="primary">发布</el-button>
+        <el-button>存入草稿</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -37,10 +41,27 @@ export default {
   data () {
     return {
       radio: 1,
-      channels: []
+      channels: [],
+      formData: {
+        title: '',
+        content: '',
+        channel_id: null,
+        cover: 0 // 默认无图
+      },
+      rules: {
+        title: [{ required: true, message: '标题内容不能为空' }],
+        content: [{ required: true, message: '文章内容不能为空' }],
+        channel_id: [{ required: true, message: '频道不能为空' }]
+      }
     }
   },
   methods: {
+    publish () {
+      this.$refs.myForm.validate((isOK) => {
+        if (isOK) {
+        }
+      })
+    },
     getChannels () {
       this.$axios({
         url: '/channels'
@@ -55,5 +76,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.publish-form {
+  margin-left: 40px;
+}
 </style>
