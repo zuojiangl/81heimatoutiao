19664390a49e98d9c79a1281b07cb82f8eec7f97
zuojianglong <3155481042@qq.com>
2019-08-25 21:33:55 +0800

    发布文章-封面组件

--- src/components/index.js
+++ src/components/index.js
@@ -5,11 +5,13 @@ import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 import { quillEditor } from 'vue-quill-editor'
+import coverImg from './publish/cover-image.vue'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)
     Vue.component('bread-crumb', breadCrumb)
     Vue.component('quill-editor', quillEditor)
+    Vue.component('cover-image', coverImg)
   }
 }
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -18,6 +18,9 @@
           <el-radio :label="-1">自动</el-radio>
         </el-radio-group>
       </el-form-item>
+      <el-form-item>
+        <cover-image :type="formData.cover.type" :images="formData.cover.images"></cover-image>
+      </el-form-item>
       <el-form-item prop="channel_id" label="频道">
         <el-select v-model="formData.channel_id" placeholder="请选择">
           <el-option
