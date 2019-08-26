d945fc3bf03bd8b8911859787935c30150a9394c
zuojianglong <3155481042@qq.com>
2019-08-26 21:15:42 +0800

    页面找不到处理

--- src/router.js
+++ src/router.js
@@ -13,6 +13,10 @@ export default new Router({
   {
     path: '/',
     redirect: '/home'
+  }, {
+    path: '*',
+    component: () =>
+                import('./views/404')
   },
   {
     path: '/home',
---
+++ src/views/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="not-find"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.not-find {
+  widows: 100%;
+  height: 100vh;
+  background-image: url('../assets/img/404.png');
+  background-size: cover;
+}
+</style>
