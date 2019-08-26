71ba52a6adcbf4f05d79eddf3a4ccd1b7c68c227
zuojianglong <3155481042@qq.com>
2019-08-26 19:20:16 +0800

    账户信息-加载数据

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,20 +1,20 @@
 <template>
-  <el-card>
+  <el-card class="parent">
     <bread-crumb slot="header">
       <template slot="title">账户管理</template>
     </bread-crumb>
-    <el-form label-width="100px">
+    <el-form label-width="100px" v-model="formData">
       <el-form-item label="用户名">
-        <el-input style="width:300px" ></el-input>
+        <el-input v-model="formData.name" style="width:300px" ></el-input>
       </el-form-item>
       <el-form-item  label="用户简介">
-        <el-input style="width:300px"></el-input>
+        <el-input v-model="formData.intro" style="width:300px"></el-input>
       </el-form-item>
       <el-form-item  label="用户邮箱">
-        <el-input style="width:300px"></el-input>
+        <el-input v-model="formData.email" style="width:300px"></el-input>
       </el-form-item>
       <el-form-item label="用户手机号">
-        <el-input style="width:300px"  disabled></el-input>
+        <el-input v-model="formData.mobile" style="width:300px"  disabled></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary">保存信息</el-button>
@@ -26,17 +26,42 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      formData: {
+        name: '',
+        intro: '',
+        photo: '',
+        email: '',
+        mobile: ''
+      }
+    }
+  },
+  methods: {
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.parent {
+  position:relative;
   .head-image {
     width: 200px;
     height: 200px;
     border-radius: 50%;
     position: absolute;
-    right: 400px;
-    top:150px;
+    left: 500px;
+    top:100px;
   }
+}
 </style>
